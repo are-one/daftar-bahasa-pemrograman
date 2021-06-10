b1902b9c49d72d7ab9fda0c5ef43914f5e2fc37c
Are1PM <arwanpriantomangidi@gmail.com>
2021-06-10 16:10:19 +0800

    create detail-lang-activity

---
+++ app/src/main/java/com/one/daftarbahasapemrograman/DetailLangActivity.kt
@@ -0,0 +1,33 @@
+package com.one.daftarbahasapemrograman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailLangActivity : AppCompatActivity() {
+
+    companion object{
+        const val EXTRA_NAME = "extra_name"
+        const val EXTRA_DESIGNED_BY = "extra_designed_by"
+        const val EXTRA_FIRST_RELEASE = "extra_first_release"
+        const val EXTRA_STABLE_RELEASE = "extra_stable_release"
+        const val EXTRA_PARADIGM = "extra_paradigm"
+        const val EXTRA_DETAIL = "extra_detail"
+        const val EXTRA_REFERENCE = "extra_reference"
+        const val EXTRA_PHOTO = "extra_photo"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_lang)
+
+        val name = intent.getStringExtra(EXTRA_NAME)
+        val designedBy = intent.getStringExtra(EXTRA_DESIGNED_BY)
+        val firstRelease = intent.getStringExtra(EXTRA_FIRST_RELEASE)
+        val stableRelease = intent.getStringExtra(EXTRA_STABLE_RELEASE)
+        val paradigm = intent.getStringExtra(EXTRA_PARADIGM)
+        val detail = intent.getStringExtra(EXTRA_DETAIL)
+        val reference = intent.getStringExtra(EXTRA_REFERENCE)
+        val photo = intent.getIntExtra(EXTRA_PHOTO, 0)
+
+    }
+}
