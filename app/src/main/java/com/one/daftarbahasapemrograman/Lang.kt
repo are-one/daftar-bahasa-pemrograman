8d8409a0fd2ebdfed401e25333ee134825bc477a
Are1PM <arwanpriantomangidi@gmail.com>
2021-06-10 15:14:11 +0800

    create model data

---
+++ app/src/main/java/com/one/daftarbahasapemrograman/Lang.kt
@@ -0,0 +1,12 @@
+package com.one.daftarbahasapemrograman
+
+data class Lang(
+    var name: String = "",
+    var designed_by: String = "",
+    var first_release: String = "",
+    var stable_release: String = "",
+    var paradigm: String = "",
+    var detail: String = "",
+    var reference: String = "",
+    var photo: Int = 0
+)
